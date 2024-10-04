823bfe6d2d6284d749fbd544928c86bd98c7df65
YQ <astrea.goh@gmail.com>
2024-10-04 13:04:37 +0800

    Merge branch properly

--- main.py
+++ main.py
@@ -26,9 +26,7 @@ class MainApplication:
         self.game_page = GamePage(self, self.show_save_score_page)  # GamePage now ends with save score
         self.high_score_page = HighScoreFrame(self)  # Initialize HighScoreFrame
         self.instruction_page = InstructionPage(self.root, self)
-
-        # Create save score page (initially hidden)
-        self.save_score_page = SaveScoreFrame(self.root, 0, self.back_to_main_menu, self.play_with_sound)  # Pass play_with_sound here too
+        self.save_score_page = None  # Placeholder for SaveScoreFrame
 
         # Bind the Escape key to exit fullscreen
         self.root.bind("<Escape>", self.exit_fullscreen)
